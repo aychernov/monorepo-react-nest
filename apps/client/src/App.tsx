ecf397035b8a174ae07e8e70260cfae75777f7a2
MaximBatalov1 <maksimka.batalov.2016@mail.ru>
2023-10-15 22:51:38 +0300

    добавил текст

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     return (
         <>
             <ProfileForm/>
-
+<h1>sdfgojsigjisg</h1>
             {/*// TODO: Вынести нижнее меню в отдельный компонент как в примере выше*/}
             <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
                 <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
